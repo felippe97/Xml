7224e2cb1e521e0550382612d1a5cde951398f5e
felippe97 <84409435+felippe97@users.noreply.github.com>
2022-09-05 15:30:36 +0200

    .

--- src/main/java/MyServer/CsvXml/Xml.java
+++ src/main/java/MyServer/CsvXml/Xml.java
@@ -1,34 +1,26 @@
 package MyServer.CsvXml;
 
-
-import java.io.IOException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import javax.xml.transform.Transformer;
-
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Document;
 
 public class Xml {
-
-	public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException {
-		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-
-		DocumentBuilder documentBuilder = builderFactory.newDocumentBuilder();
-
-		Document document = documentBuilder.newDocument();
-
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		DOMSource domSource = new DOMSource();
-		StreamResult streamResult = new StreamResult("/file.xml");
-		transformer.transform(domSource, streamResult);
+	
+	protected DocumentBuilderFactory documentBuilderFactory = null;
+	
+	protected DocumentBuilder documentBuilder = null;
+	
+	public int convertFile(String csvFileName, String xmlFileName) {
+		
+		
+		
+		return 0;
+		
 	}
+  public static void main(String[] args) {
+	  
+	  
+	  
+	  
+	  
+}
 }
