de2500aa856fc9eae83d9a584e9ce2f7113ce5ea
felippe97 <84409435+felippe97@users.noreply.github.com>
2022-09-06 13:44:11 +0200

    .

--- src/main/java/MyServer/CsvXml/Book.java
+++ src/main/java/MyServer/CsvXml/Book.java
@@ -26,33 +26,35 @@ public class Book {
 			Element docElement = document.createElement("Book");
 			document.appendChild(docElement);
 			
-			 docElement.appendChild(getBook(document, "1", "Kniha", "g1", "1000"));
-			 docElement.appendChild(getBook(document, "2", "Kniha2", "g2", "2000"));
-			 docElement.appendChild(getBook(document, "3", "Kniha3", "g3", "3000"));
-			docElement.appendChild(getBook(document, "4", "Kniha4", "g4", "10"));
-			  Transformer crunchifyTransformer = TransformerFactory.newInstance().newTransformer();
-	            Transformer crunchifyTransformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			 docElement.appendChild((Node) getBook(document, "1", "Kniha", "g1", "1000"));
+			 docElement.appendChild((Node) getBook(document, "2", "Kniha2", "g2", "2000"));
+			 docElement.appendChild((Node) getBook(document, "3", "Kniha3", "g3", "3000"));
+			docElement.appendChild((Node) getBook(document, "4", "Kniha4", "g4", "10"));
+			  Transformer transformer = TransformerFactory.newInstance().newTransformer();
+	            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 	         
 	            DOMSource source = new DOMSource(document);
 	            StreamResult console = new StreamResult(System.out);
-	            crunchifyTransformer.transform(source, console);
-	            System.out.println( "\nVýukový program od Crunchify. XML DOM bol úspešne vytvorený.." ) ;
+	            transformer.transform(source, console);
+	           
 		} catch (TransformerException | ParserConfigurationException e ) {   
             e. printStackTrace();
 			// TODO: handle exception
 		}
 	}
 
+	
+
 	private static Object getBook(Document document, String id, String name, String gender, String price) {
 		Element fyBook = document.createElement("Book");
 		fyBook.setAttribute("id", id);
-		fyBook.appendChild(getCrunchifyCompanyElements(document, fyBook, "Name", name));
-		fyBook.appendChild(getCrunchifyCompanyElements(document, fyBook, "Gender", gender));
-		fyBook.appendChild(getCrunchifyCompanyElements(document, fyBook, "Price", price));
+		fyBook.appendChild(getfyBookElements(document, fyBook, "Name", name));
+		fyBook.appendChild(getfyBookElements(document, fyBook, "Gender", gender));
+		fyBook.appendChild(getfyBookElements(document, fyBook, "Price", price));
 		return fyBook;
 	}
 
-	private static Node getCrunchifyCompanyElements(Document doc, Element element, String name, String value) {
+	private static Node getfyBookElements(Document doc, Element element, String name, String value) {
 		Element node = doc.createElement(name);
 		node.appendChild(doc.createTextNode(value));
 		return node;
