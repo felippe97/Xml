32045eaacb796807389b186045ab1d48e2b2826a
felippe97 <84409435+felippe97@users.noreply.github.com>
2022-09-07 15:13:34 +0200

    Update Xml.java

--- src/main/java/MyServer/Xml/Xml.java
+++ src/main/java/MyServer/Xml/Xml.java
@@ -21,8 +21,10 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
@@ -31,12 +33,10 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+public class Xml {
+	public static void main(String[] args)
+			throws TransformerFactoryConfigurationError, TransformerException, ParserConfigurationException {
 
-public class Xml{
-	public static void main(String[] args) {
-		
-		
-		
 		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
 
 		DocumentBuilder documentBuilder = builderFactory.newDocumentBuilder();
@@ -46,16 +46,12 @@ public class Xml{
 			Document document = documentBuilder.newDocument();
 			Element docElement = document.createElement("books");
 			document.appendChild(docElement);
-		
-				
-			
-			
-			
-			  docElement.appendChild(getBook(document, "1", "Kniha", "g1", "25"));
-			  docElement.appendChild(getBook(document, "2", "Kniha2", "g2", "40"));
-			  docElement.appendChild(getBook(document, "3", "Kniha3", "g3", "15"));
-			  docElement.appendChild(getBook(document, "4", "Kniha4", "g4", "10"));
-			 
+
+			docElement.appendChild(getBook(document, "1", "Kniha", "g1", "25"));
+			docElement.appendChild(getBook(document, "2", "Kniha2", "g2", "40"));
+			docElement.appendChild(getBook(document, "3", "Kniha3", "g3", "15"));
+			docElement.appendChild(getBook(document, "4", "Kniha4", "g4", "10"));
+
 			Transformer transformer = TransformerFactory.newInstance().newTransformer();
 			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
@@ -63,35 +59,25 @@ public class Xml{
 			StreamResult console = new StreamResult(System.out);
 			transformer.transform(source, console);
 
-		}catch(ParserConfigurationException e) {
+		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
-			
+
 		}
-	
 
+	}
 
-
-	
-	  private static Object getBook(Document document, String id, String name, String gender, String price) { 
-	  Element book = document.createElement("Book");
-	  book.setAttribute("id", id); 
-	  book.appendChild(getfyBookElements(document, book, "Name", name));
-	  book.appendChild(getfyBookElements(document, book,"Gender", gender));
-	  book.appendChild(getfyBookElements(document, book, "Price", price)); 
-	  return book; 
-	  }
-	 
-
-	
-	 
-}
-
-
+	private static Node getBook(Document document, String string, String string2, String string3, String string4) {
+		Element book = document.createElement("Book");
+		book.setAttribute("id", string);
+		book.appendChild(getfyBookElements(document, book, "Name", string2));
+		book.appendChild(getfyBookElements(document, book, "Gender", string3));
+		book.appendChild(getfyBookElements(document, book, "Price", string4));
+		return book;
+	}
 
 	private static Node getfyBookElements(Document document, Element element, String name, String value) {
-		  Element node = document.createElement(name);
-		  node.appendChild(document.createTextNode(value)); 
-		  return node;
+		Element node = document.createElement(name);
+		node.appendChild(document.createTextNode(value));
+		return node;
 	}
 }
-
